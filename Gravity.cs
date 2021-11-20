7d0d49c9c1fc093eab2a5e6c2405d34d3689b400
Liuk23 <ldvcoding@gmail.com>
2021-11-20 14:15:23 +0100

    gravity.cs

---
+++ Gravity.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 velocity;
+    float gravity = -9.81f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Gravity going on...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
