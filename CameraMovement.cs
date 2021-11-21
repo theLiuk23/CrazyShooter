c422b4e16f8628d980a742c65307bece88066eae
Liuk23 <ldvcoding@gmail.com>
2021-11-21 16:57:36 +0100

    null

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public CharacterController controller;
-
     // separator in the unity inspector
     [Header("Look sensitivity")]
-    public float keyboardSens = 20f;
     public float mouseSens = 300f;
 
     float mouseX = 0f;
@@ -23,29 +20,11 @@ public class CameraMovement : MonoBehaviour
 
     void LateUpdate()
     {
-        // QWEASD MOVEMENT
-        float keyboardX = Input.GetAxis("Horizontal");
-        float keyboardZ = Input.GetAxis("Vertical");
-        float keyboardY = GetUpDownMovement();
-
-        Vector3 move = transform.right * keyboardX + transform.up * keyboardY + transform.forward * keyboardZ;
-        controller.Move(move * keyboardSens * Time.deltaTime);
-
-
-
-
         // MOUSE MOVEMENT
         mouseX += Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
         mouseY += -Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
         mouseY = Mathf.Clamp(mouseY, -90f, 90f);
 
         transform.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
-    }
-
-    float GetUpDownMovement()
-    {
-        if (Input.GetKey(KeyCode.E)) return 1f;
-        if (Input.GetKey(KeyCode.Q)) return -1f;
-        return 0;
     }
 }
---
+++ PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float keyboardSens = 20f;
+    public float mouseSens = 300f;
+    public float height = 3f;
+
+    float gravity = -9.81f * 3;
+    bool isGrounded = true;
+
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, groundMask);
+        if (isGrounded && velocity.y < 0) velocity.y = -1f;
+
+        // WASD MOVEMENT
+        float keyboardX = Input.GetAxis("Horizontal");
+        float keyboardZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * keyboardX * keyboardSens + transform.forward * keyboardZ * keyboardSens;
+        controller.Move(move * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+
+
+
+        // JUMPING
+        if (Input.GetButtonDown("Jump") && isGrounded) velocity.y = Mathf.Sqrt(height * -2f * gravity);
+    }
+}
