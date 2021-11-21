7798edc24571d287fcb6dcdd231dba183a9e2207
Liuk23 <ldvcoding@gmail.com>
2021-11-21 21:03:42 +0100

    added transition

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -8,14 +8,19 @@ public class CameraMovement : MonoBehaviour
     [Header("Look sensitivity")]
     public float mouseSens = 300f;
 
+    [Header("Objects")]
+    public Transform legs;
+    public Transform arms;
+    public Transform body;
+    public Transform head;
     float mouseX = 0f;
     float mouseY = 0f;
 
     void Start()
     {
-        transform.Rotate(new Vector3(0f, 0f, 0f));
         // locks the cursor
         Cursor.lockState = CursorLockMode.Locked;
+        transform.Rotate(new Vector3(0f, 0f, 0f));
     }
 
     void LateUpdate()
@@ -23,8 +28,17 @@ public class CameraMovement : MonoBehaviour
         // MOUSE MOVEMENT
         mouseX += Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
         mouseY += -Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
-        mouseY = Mathf.Clamp(mouseY, -90f, 90f);
 
+        // locking the rotation
+        mouseY = Mathf.Clamp(mouseY, -60f, 80);
+
+        // rotating player
+        head.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
+        arms.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
+        legs.rotation = Quaternion.Euler(0f, mouseX, 0f);
+        body.rotation = Quaternion.Euler(0f, mouseX, 0f);
+
+        // rotating camera
         transform.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
     }
 }
--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -5,44 +5,40 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform cameraTransform;
+    public Animator runAnimation;
     public float keyboardSens = 20f;
     public float mouseSens = 300f;
     public float height = 3f;
 
     float gravity = -9.81f * 3;
-    bool isGrounded = true;
+    public bool isGrounded;
 
     public Transform groundCheck;
     public LayerMask groundMask;
     public Vector3 velocity;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, groundMask);
         if (isGrounded && velocity.y < 0) velocity.y = -1f;
 
-        // WASD MOVEMENT
+        // WASD MOVEMENT //
         float keyboardX = Input.GetAxis("Horizontal");
         float keyboardZ = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * keyboardX * keyboardSens + transform.forward * keyboardZ * keyboardSens;
+        if (keyboardZ > 0) runAnimation.SetBool("isRunning", true);
+        else runAnimation.SetBool("isRunning", false);
+
+        Vector3 move = cameraTransform.right * keyboardX * keyboardSens + cameraTransform.forward * keyboardZ * keyboardSens;
         controller.Move(move * Time.deltaTime);
+
+        // GRAVITY //
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
 
-
-        // TODO: movement based on local orientation
-
-
-        // JUMPING
+        // JUMPING //
         if (Input.GetButtonDown("Jump") && isGrounded) velocity.y = Mathf.Sqrt(height * -2f * gravity);
     }
 }
